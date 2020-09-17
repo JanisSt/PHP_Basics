c6733cee822e94327fd5aa2a1881b49fb3da63b1
Janis Stals <50199750+JanisSt@users.noreply.github.com>
2020-09-18 02:30:42 +0300

    Update Exercise6.php

--- loops/Exercise6.php
+++ loops/Exercise6.php
@@ -1,16 +1,25 @@
 <?php
 
-Echo "Enter a size of the Pyramid: " . PHP_EOL;
-$x = (int)(readline());
-for($i = 1; $i<=$x; $i++){
-    for($t = 0; $t < (4 * ($x - $i)); $t++){
-        echo '/';
-    }
-    for ($t = 0; $t < (8 * ($i -1)); $t++){
-        echo"*";
+class Pyramid
+{
+    public function Draw($x)
+    {
+        for ($i = 1; $i <= $x; $i++) {
+            for ($t = 0; $t < (4 * ($x - $i)); $t++) {
+                echo '/';
             }
-    for ($t = 0; $t <(4 * ($x - $i)); $t++){
-        echo chr(92);
+            for ($t = 0; $t < (8 * ($i - 1)); $t++) {
+                echo "*";
+            }
+            for ($t = 0; $t < (4 * ($x - $i)); $t++) {
+                echo chr(92);
+            }
+            echo PHP_EOL;
+        }
     }
-    echo PHP_EOL;
 }
+
+echo "Enter a size of the Pyramid: " . PHP_EOL;
+$x = (int)(readline());
+
+Pyramid::Draw($x);
